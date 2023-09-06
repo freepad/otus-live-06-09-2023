c4de75fab1eec9dc228d9ba46cf8fee4ba96ada8
Damir Rysaev <freepad@ya.ru>
2023-09-06 16:20:09 +0300

    add example api test

--- test/sum.test.js
+++ test/sum.test.js
@@ -1,4 +1,5 @@
 const sum = require('./sum')
+const request = require('supertest');
 
 test('adds 1 + 2 to equal 3', () => {
   expect(sum(1, 2)).toBe(3)
@@ -6,4 +7,17 @@ test('adds 1 + 2 to equal 3', () => {
 
 test('adds 2 + 2 to equal 4', () => {
   expect(sum(2, 2)).toBe(4)
+})
+
+test('success create user', async () => {
+  const response = await request('https://reqres.in')
+    .post('/api/users')
+    .send({
+      "name": "morpheus",
+      "job": "leader"
+  })
+
+    expect(response.status).toBe(201)
+    expect(response.body.name).toBe("morpheus");
+    expect(response.body.job).toBe("leader");
 })
