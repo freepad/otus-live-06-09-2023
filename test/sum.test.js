ce84e1cc8a2a9dca5944bcd862a3ca73508b3d8c
Damir Rysaev <freepad@ya.ru>
2023-09-06 16:26:48 +0300

    add faker

--- test/sum.test.js
+++ test/sum.test.js
@@ -1,5 +1,6 @@
 const sum = require('./sum')
 const request = require('supertest');
+const { faker } = require('@faker-js/faker');
 
 test('adds 1 + 2 to equal 3', () => {
   expect(sum(1, 2)).toBe(3)
@@ -10,14 +11,16 @@ test('adds 2 + 2 to equal 4', () => {
 })
 
 test('success create user', async () => {
+  const randomName = faker.person.fullName(); 
+
   const response = await request('https://reqres.in')
     .post('/api/users')
     .send({
-      "name": "morpheus",
+      "name": randomName,
       "job": "leader"
   })
 
     expect(response.status).toBe(201)
-    expect(response.body.name).toBe("morpheus");
+    expect(response.body.name).toBe(randomName);
     expect(response.body.job).toBe("leader");
 })
